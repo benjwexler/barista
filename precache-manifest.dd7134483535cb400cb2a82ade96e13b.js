0d981f4f0596ca51817ec43e4b2422e11bee2f46
Ben Wexler <mixedywex@gmail.com>
2020-02-29 23:50:59 -0500

    Updates

---
+++ precache-manifest.dd7134483535cb400cb2a82ade96e13b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781c7214cae9fe40f4a9c88d73a21cd4",
+    "url": "/coffee-makers/index.html"
+  },
+  {
+    "revision": "a21ca464814416b87ac8",
+    "url": "/coffee-makers/static/css/main.8149653c.chunk.css"
+  },
+  {
+    "revision": "193088fa6e2291cd7d78",
+    "url": "/coffee-makers/static/js/2.2609717a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/coffee-makers/static/js/2.2609717a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21ca464814416b87ac8",
+    "url": "/coffee-makers/static/js/main.0912aa65.chunk.js"
+  },
+  {
+    "revision": "fa72f92987194922bb12",
+    "url": "/coffee-makers/static/js/runtime-main.0cc9b083.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coffee-makers/static/media/logo.5d5d9eef.svg"
+  }
+]);
